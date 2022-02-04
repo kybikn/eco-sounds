564649f93481e6e97eb5ed926cf04feb8d709e31
Kybik <82117054+kybikn@users.noreply.github.com>
2022-02-04 23:20:40 +0300

    feat: add nav, audio

--- js30-1.1-eco-sounds/script.js
+++ js30-1.1-eco-sounds/script.js
@@ -1,17 +1,11 @@
+const button = document.querySelector("button");
+const audio = document.querySelector("audio");
+const playBtn = document.querySelector(".play");
+const videoContainer = document.querySelector(".video-container");
 
-// const audio = document.querySelector('audio');
-const playBtn = document.querySelector('.play-btn');
-const pauseBtn = document.querySelector('.pause-btn');
-const audio = new Audio();
-// let isPlay = false;
-
-
-playBtn.addEventListener('click', playAudio);
-pauseBtn.addEventListener('click', pauseAudio);
-
+let isPlay = false;
 
 function playAudio() {
-  audio.src = './assets/audio/drozd.mp3';
   audio.currentTime = 0;
   audio.play();
 }
@@ -20,23 +14,85 @@ function pauseAudio() {
   audio.pause();
 }
 
-
-
-
-
-
-const button = document.querySelector('button');
-
 function toggleBtn() {
-  button.classList.toggle('pause');
+  if (!isPlay) {
+    playBtn.classList.add("pause");
+    audio.play();
+    isPlay = true;
+  } else {
+    playBtn.classList.remove("pause");
+    audio.pause();
+    isPlay = false;
+  }
 }
-button.addEventListener('click', toggleBtn);
 
+playBtn.addEventListener("click", toggleBtn);
 
+const linkItems = document.querySelectorAll(".nav-link");
+const linkItemsBox = document.querySelector(".nav");
+const linkImages = document.querySelectorAll(".portfolio-image");
 
+linkItemsBox.addEventListener("click", changeAudio);
 
+function changeAudio(event) {
+  if (event.target.classList.contains("nav-link")) {
+    let bird = event.target.dataset.bird;
 
-// window.addEventListener('click', function(event) {
+    videoContainer.style.backgroundImage = `url('./assets/img/${bird}.jpg')`;
+
+    linkItems.forEach((element) => element.classList.remove("active"));
+
+    event.target.classList.add("active");
+
+    audio.src = `./assets/audio/${bird}.mp3`;
+    playBtn.classList.add("pause");
+    audio.play();
+    isPlay = true;
+  }
+}
+
+// playBtn.addEventListener(
+//   "click",
+//   () => {
+//     if (audio.paused) {
+//       playBtn.classList.remove("play");
+//       playBtn.classList.add("pause");
+//       audio.play();
+//     } else {
+//       playBtn.classList.remove("pause");
+//       playBtn.classList.add("play");
+//       audio.pause();
+//     }
+//   },
+//   false
+// );
+
+// let playlist = [
+//   'solovey.mp3',
+//   'drozd.mp3',
+//   'zarynka.mp3',
+//   'javoronok.mp3',
+//   'slavka.mp3',
+// ];
+
+// const playNum = 0;
+
+// function playNext('çlick', function(event) {
+//   for (let i = 0; i <= playlist.length; i++) {
+
+//   }
+// playAudio();
+
+// function playPrev();
+
+// function playRound() {
+//   if (playNum < 0) {
+//     return playlist.length - 1 playNum[i]
+// } else {
+//   return playlist[0]  playNum[0]
+// };
+
+// playBtn.addEventListener('click', function(event) {
 //   if (!isPlay) {
 //     audio.src = './assets/audio/drozd.mp3';
 //     audio.currentTime = 0;
@@ -46,76 +102,9 @@ button.addEventListener('click', toggleBtn);
 //     }
 // }
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const audio = document.querySelector('audio');
-// const playBtn = document.querySelector('video-button);
-// const pauseBtn = document.querySelector('video-button.active');
-// const myAudio = new Audio('./assets/audio/drozd.mp3');
-// let isPlay = false;
-
-
-// function playAudio() {
-//   audio.src = './assets/audio/drozd.mp3';
-//   audio.currentTime = 0;
-//   audio.play();
-// }
-// function pauseAudio() {
-//   audio.pause();
-// }
-// playBtn.addEventListener('click', playAudio);
-// pauseBtn.addEventListener('click', pauseAudio);
-
-
-
-// function result (event) 
-
-//   if (event.target.classList.contains("video-button")) {
-// if(!isPlay) 
-
-
-
-
-// const audio = document.querySelector('audio');
-// const playBtn = document.querySelector('.play-btn');
-// const pauseBtn = document.querySelector('.pause-btn');
-
-// function playAudio() {
-//   audio.currentTime = 0;
-//   audio.play();
-// }
-
-// function pauseAudio() {
-//   audio.pause();
-// }
-
-// playBtn.addEventListener('click', playAudio);
-// pauseBtn.addEventListener('click', pauseAudio);
-
-
-
-
-
-
-
-
-// const audio = document.querySelector('audio');
-// const playBtn = document.querySelector('.video-button');
-// const pauseBtn = document.querySelector('.video-button.active');
-
-
-// playBtn.addEventListener('click', playVideo);
-// pauseBtn.addEventListener('click', pauseVideo);
+// function result (event)
+//   if (event.target.classList.contains(".play")) {
+// if(!isPlay)
 
 // function playVideo() {
 //   audio.currentTime = 0;
@@ -126,23 +115,14 @@ button.addEventListener('click', toggleBtn);
 //   video.pause();
 // }
 
-
-
-
-
-
-
-
-
-
-// const videoButton = document.querySelector(".video-button");
+// const videoButton = document.querySelector('.play');
 // videoButton.addEventListener('click', changeButton);
 
 // function changeButton() {
 //   videoArray.forEach((selector) => {
 //     const elements = document.querySelectorAll(selector);
 //     elements.forEach((element) => {
-//       element.classList.toggle('pause')
+//       element.classList.toggle('.pause')
 //     })
 //   })
 // }
